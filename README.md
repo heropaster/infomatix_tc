14bb49cd022562ecb0bdaa29cab324c1ec89e255
Heropaster <heropasta@mail.ru>
2023-09-22 20:11:20 +0700

    vite init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Test case for Infomatix
+
+**Stack** :
+
+1. React
+2. TypeScript
+3. SCSS
---
+++ src/components/ProductsTable.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const ProductsTable = () => {
+    
+}   
+export default ProductsTable
